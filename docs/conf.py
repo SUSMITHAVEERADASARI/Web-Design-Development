66b723a8f418e2be62715fdad35b44d57a30de5a
vijay kumar <vijay10022@gmail.com>
2020-05-04 11:49:33 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,13 +30,12 @@ release = '1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-source_suffix = '.rst'
+source_suffix = ['.rst','.md']
 master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
